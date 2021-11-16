8413ce496a24d89af6562b7943ecef26ed486665
Crowdbotics <team@crowdbotics.com>
2021-11-16 20:23:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pot_luck_app_32023",
+  url: "https://pot_luck_app_32023.botics.co",
+  api: "https://pot_luck_app_32023.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
